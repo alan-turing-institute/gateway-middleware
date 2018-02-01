1297210c0a6c9433f2cc78eea6278ea934940e4d
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2018-02-01 13:59:50 +0000

    Make the initDatabase.sh script be more sensible

--- initDatabase.sh
+++ initDatabase.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-name=$(docker-compose run -p "8082:5432" postgres)
+docker-compose run -d -p "8082:5432" postgres
 sleep 10
 python create_and_mint_case_using_stores.py
 sleep 10
-docker stop "$name"
+docker-compose down
