a65b131c1216b37c80ccf3b148b35519187e3ed2
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2018-02-07 17:01:45 +0000

    Setup test decorators

--- routes/__init__.py
+++ routes/__init__.py
@@ -10,6 +10,6 @@ def setup_routes(api):
     Set up the routes for these api end points
     """
     api.add_resource(CasesApi, '/case')
-    api.add_resource(CaseApi, '/case/<case_id>')
+    api.add_resource(CaseApi, '/case/<int:case_id>')
     api.add_resource(JobsApi, '/job')
-    api.add_resource(JobApi, '/job/<job_id>')
+    api.add_resource(JobApi, '/job/<int:job_id>')
--- tests/test_cases.py
+++ tests/test_cases.py
@@ -12,26 +12,51 @@ from routes import setup_routes, CasesApi, CaseApi
 
 from .create_and_mint_case_using_stores import set_up_test_database
 
+from pytest import fixture
 
-def test_something():
+from .decorators import request_context
+
+
+@fixture(scope="module")
+def app():
+    """
+    Setup the flask app context I hope
+    """
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
     app.testing = True
 
     init_database(app)
-
     init_marshmallow(app)
 
     api = Api(app)
 
     setup_routes(api)
-
     with app.app_context():
         set_up_test_database()
-        with app.test_request_context('a', method="GET"):
-            print(CaseApi().dispatch_request(1))  # Manually put in the case id
+    return app
 
-        with app.test_request_context("/case", method="GET"):
-            # This fails because it cannot generate the self URL in the links section
-            print(CasesApi().dispatch_request())
+
+def test_something(app):
+    """
+    Test that a basic query of gets lots of cases
+    """
+    with app.app_context():
+        with app.test_request_context('a', method="GET"):
+            result = CaseApi().dispatch_request(1)
+            print(result.data)
+
+
+@request_context("/case?per_page=1", method="GET")
+def test_1_per_page(app):
+    print(app)
+    result = CasesApi().dispatch_request()
+    assert(len(result.data) == 1)
+
+
+@request_context("/case", method="GET")
+def test_per_page(app):
+    print(app)
+    result = CasesApi().dispatch_request()
+    assert(len(result.data) == 3)
