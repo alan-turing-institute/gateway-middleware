98ee6e009bfe25e00218296a31f0556f8fe0a942
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2018-03-26 16:54:51 +0100

    Add a flask jobstatus to url converter

--- connection/constants.py
+++ connection/constants.py
@@ -1,18 +1,37 @@
 """
-Constants module
+Constants and utilites module
 """
 
 from enum import Enum
+from werkzeug.routing import BaseConverter
 
 JOB_MANAGER_URL = 'http://job-manager:5001'
 
 
 class JobStatus(Enum):
     """
-    Job status enum
+    Job status enum. Variable must always be UPPERCASE.
     """
+    FAILED = 'Failed'
     NOT_STARTED = 'Not Started'
     QUEUED = 'Queued'
+
+
+class StatusConverter(BaseConverter):
+    """
+    Flask url converter for Status enums
+    """
+    def to_python(self, value: str):
+        """
+        Convert the string from the URL to an enum
+        """
+        return JobStatus[value.upper()]
+
+    def to_url(self, value: JobStatus):
+        """
+        Convert the enum to the url representation
+        """
+        return value.name
 
 
 class RequestStatus(Enum):
