cf6c8f9651430b7161b36b8cd1f07319d82f2741
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2018-04-10 19:01:36 +0100

    Fix broken tests

--- routes/fake_routes.py
+++ routes/fake_routes.py
@@ -6,17 +6,13 @@ from flask_restful import Resource
 
 from tests.create_and_mint_case_using_stores import set_up_test_database
 
-from tests.openfoam_test_data import upload_dambreak_test
 
 class TestData(Resource):
     """
     Class to be used for generating fake data
     """
-    def post(self,test_id):
+    def post(self):
         """
         This just creates the default fake data
         """
-        if test_id == 0:
-            set_up_test_database()
-        else:
-            upload_dambreak_test()
+        set_up_test_database()
