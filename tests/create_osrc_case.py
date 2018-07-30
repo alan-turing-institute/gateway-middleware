5bc46412657f17daca7bf18d7e2311384b58cba5
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2018-07-30 14:59:17 +0100

    Test case that uses the read from json case loader and can clear the database

---
+++ tests/create_osrc_case.py
@@ -0,0 +1,32 @@
+"""
+Create a test icoFoam cavity case,
+using components from the "phase store".
+"""
+
+from dataloading.case_json_loader import read_case_from_json
+from connection.models import Case, Job, db
+
+def set_up_osrc_testdata():
+    """
+    Make a osrc case and commit it to the database
+    """
+    session = db.session
+    # make osrc case
+    osrc = read_case_from_json('resources/osrc.json')
+
+    session.add(osrc)
+    session.commit()
+
+def clear_db():
+    """
+    Clear the database from all the junk
+    """
+    session = db.session
+
+    jobs = Job.query.all()
+    for job in jobs:
+        session.delete(job)
+    cases = Case.query.all()
+    for case in cases:
+        session.delete(case)
+    session.commit()
