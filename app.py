c1cf02ee5381bcf605f01efacc0930d1d291f857
masonlr <l.mason@imperial.ac.uk>
2018-09-10 13:37:27 +0100

    test: Turn threaded=True option on

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 set_up_routes(app, api)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000, threaded=True)
