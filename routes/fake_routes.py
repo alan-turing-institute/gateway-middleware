72031c9e723c473110a45fc60f5be58b2df6f49f
masonlr <masonlr@unimelb.edu.au>
2018-09-30 18:21:29 +0100

    feat: Fill out flushout case

--- routes/fake_routes.py
+++ routes/fake_routes.py
@@ -5,6 +5,7 @@ from flask_restful import Resource
 
 from tests.create_cavity_case import set_up_cavity_testdata
 from tests.create_damBreak_case import set_up_dambreak_testdata
+from tests.create_flushout_case import set_up_flushout_testdata
 
 
 class TestData(Resource):
@@ -23,6 +24,9 @@ class TestData(Resource):
         messages.append(message)
 
         message = set_up_cavity_testdata()
+        messages.append(message)
+
+        message = set_up_flushout_testdata()
         messages.append(message)
 
         return {"status": "success", "messages": messages, "errors": errors}
